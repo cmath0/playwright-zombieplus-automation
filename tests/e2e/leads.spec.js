b6d1bad36aec11cbc6f08d06a773e459571fd3d3
Matheus Cardoso da Silva <matheus97_mcs@hotmail.com>
2025-11-26 14:00:43 -0300

    Automatizar cenário de criação de filmes

--- tests/e2e/leads.spec.js
+++ tests/e2e/leads.spec.js
@@ -21,7 +21,7 @@ test('deve cadastrar um lead na fila de espera', async ({ page }) => {
   await landingPage.submitLeadForm(leadName, leadEmail)
 
   const message = 'Agradecemos por compartilhar seus dados conosco. Em breve, nossa equipe entrará em contato!'
-  await toast.haveText(message)
+  await toast.containText(message)
 });
 
 test('não deve cadastrar quando o email já existe', async ({ page, request }) => {
@@ -42,7 +42,7 @@ test('não deve cadastrar quando o email já existe', async ({ page, request }) 
   await landingPage.submitLeadForm(leadName, leadEmail)
 
   const message = 'O endereço de e-mail fornecido já está registrado em nossa fila de espera.'
-  await toast.haveText(message)
+  await toast.containText(message)
 });
 
 test('não deve cadastrar ao informar um email inválido', async ({ page }) => {
--- tests/pages/MoviesPage.js
+++ tests/pages/MoviesPage.js
@@ -10,4 +10,28 @@ export class MoviesPage {
         await this.page.waitForLoadState('networkidle')
         await expect(this.page).toHaveURL(/.*movies/)
     }
+
+    async create(title, overview, company, release_year) {
+        await this.page.locator('a[href$="register"]').click()
+
+        await expect(this.page.locator('header h1')).toHaveText('Cadastrar novo Filme')
+
+        await this.page.getByLabel('Titulo do filme').fill(title)
+        await this.page.getByLabel('Sinopse').fill(overview)
+        
+        await this.page.locator('#select_company_id .react-select__indicator')
+            .click()
+        await this.page.locator('.react-select__option')
+            .filter({ hasText: company })
+            .click()
+        
+        await this.page.locator('#select_year .react-select__indicator')
+            .click()
+        await this.page.locator('.react-select__option')
+            .filter({ hasText: release_year })
+            .click()
+        
+        await this.page.getByRole('button', { name: 'Cadastrar' })
+            .click()
+    }
 }
