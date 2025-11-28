88e1502bd6fae6c69a786121b3f25ed5b728058a
Matheus Cardoso da Silva <matheus97_mcs@hotmail.com>
2025-11-28 11:41:04 -0300

    Automatizar cenário de remoção de filme do catálogo

--- tests/e2e/movies.spec.js
+++ tests/e2e/movies.spec.js
@@ -14,6 +14,17 @@ test('deve poder cadastrar um novo filme', async ({ page }) => {
     await page.popup.haveText(`O filme '${movie.title}' foi adicionado ao catálogo.`)
 })
 
+test('deve poder remover um filme', async ({ page, request }) => {
+    const movie = data.to_remove
+
+    await executeSQL(`DELETE FROM movies WHERE title = '${movie.title}'`)
+    await request.api.postMovie(movie)
+
+    await page.login.do('admin@zombieplus.com', 'pwd123', 'Admin')
+    await page.movies.remove(movie.title)
+    await page.popup.haveText('Filme removido com sucesso.')
+})
+
 test('não deve cadastrar quando o título já está cadastrado', async ({ page, request }) => {
     const movie = data.duplicate
 
